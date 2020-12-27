8d9ace03125f68476c19a7d8f6e28ee114513384
Abe <abhi.rawat456@gmail.com>
2020-12-27 22:43:00 +0530

    Added Caching to Trending Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import GameDetails from "./components/GameDetails";
 import Navbar from "./components/Navbar";
@@ -10,18 +11,21 @@ import Trending from "./components/Trending";
 import RootReducer from "./store/reducers";
 
 const store = createStore(RootReducer);
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={Trending} exact />
-          <Route path="/search" component={Search} />
-          <Route path="/:id" component={GameDetails} />
-        </Switch>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/search" component={Search} />
+            <Route path="/:id" component={GameDetails} />
+          </Switch>
+        </BrowserRouter>
+      </QueryClientProvider>
     </Provider>
   );
 }
--- src/components/Trending/Trending.tsx
+++ src/components/Trending/Trending.tsx
@@ -1,11 +1,7 @@
-import React, { Component } from "react";
+import React, { ReactElement } from "react";
+import { useQuery } from "react-query";
+
 import GameCard from "./GameCard";
-
-interface Props {}
-
-interface State {
-  games: Game[];
-}
 
 export interface Game {
   name: string;
@@ -30,52 +26,53 @@ export interface Game {
   }[];
 }
 
-export default class Trending extends Component<Props, State> {
-  state = {
-    games: [],
-  };
-
-  async componentDidMount() {
-    console.log("[Trending] CDM");
+export default function Trending(): ReactElement {
+  const getData = async () => {
     const res = await fetch(
       "https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added"
     );
-    const data = await res.json();
-    const results: Game[] = [];
-    for (const obj of data.results) {
-      const game: Game = {
-        name: obj.name,
-        playtime: obj.playtime,
-        released: obj.released,
-        background_image: obj.background_image,
-        rating: obj.rating,
-        id: obj.id,
-        parent_platforms: obj.parent_platforms,
-        genres: obj.genres,
-        short_screenshots: obj.short_screenshots,
-      };
-      results.push(game);
-    }
-    this.setState({
-      games: results,
-    });
+    return res.json();
+  };
+
+  const { isLoading, error, data } = useQuery("games", getData);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
 
-  render() {
-    return (
-      <div className="dark:bg-black dark:text-white min-h-screen">
-        <div className="max-w-screen-lg mx-auto py-6 px-4">
-          <h1 className="font-bold text-6xl">New and trending</h1>
-          <p className="font-normal my-3">
-            Based on player counts and release date
-          </p>
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {this.state.games.map((game: Game) => (
-              <GameCard key={game.id} game={game} />
-            ))}
-          </div>
+  if (error) {
+    return <p>Failed to load data</p>;
+  }
+
+  const results: Game[] = [];
+  for (const obj of data.results) {
+    const game: Game = {
+      name: obj.name,
+      playtime: obj.playtime,
+      released: obj.released,
+      background_image: obj.background_image,
+      rating: obj.rating,
+      id: obj.id,
+      parent_platforms: obj.parent_platforms,
+      genres: obj.genres,
+      short_screenshots: obj.short_screenshots,
+    };
+    results.push(game);
+  }
+
+  return (
+    <div className="dark:bg-black dark:text-white min-h-screen">
+      <div className="max-w-screen-lg mx-auto py-6 px-4">
+        <h1 className="font-bold text-6xl">New and trending</h1>
+        <p className="font-normal my-3">
+          Based on player counts and release date
+        </p>
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {results.map((game: Game) => (
+            <GameCard key={game.id} game={game} />
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
