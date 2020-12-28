7d9b471ae3f0598d79568c681db62721eee3b5e7
Abe <abhi.rawat456@gmail.com>
2020-12-28 17:36:16 +0530

    Added Firebase

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { Dispatch } from "redux";
 
 import DarkModeAction from "../../store/actions/DarkMode";
 import { DarkModeState } from "../../store/reducers/types";
+import firebase from "../../firebase/firebase";
 
 interface Props {
   toggleDarkMode: () => void;
@@ -29,6 +30,17 @@ class Navbar extends Component<Props, State> {
       isDropDownOpen: !prevState.isDropDownOpen,
     }));
   };
+
+  componentDidMount() {
+    console.log("[Navbar] CDM");
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user);
+      } else {
+        console.log("EMPTY");
+      }
+    });
+  }
 
   render() {
     return (
@@ -57,9 +69,9 @@ class Navbar extends Component<Props, State> {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
                   />
                 </svg>
@@ -83,9 +95,9 @@ class Navbar extends Component<Props, State> {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
                   />
                 </svg>
@@ -103,9 +115,9 @@ class Navbar extends Component<Props, State> {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
@@ -118,9 +130,9 @@ class Navbar extends Component<Props, State> {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 8h16M4 16h16"
                   />
                 </svg>
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyCKRzfVtPNKoaFRl9xhdHFTEM5C81_hkfE",
+  authDomain: "gameigo-1890c.firebaseapp.com",
+  projectId: "gameigo-1890c",
+  storageBucket: "gameigo-1890c.appspot.com",
+  messagingSenderId: "581146013271",
+  appId: "1:581146013271:web:6580db0f745920e5edbd12",
+  measurementId: "G-HW85YXWZ6Q",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
