c31200b7dc3a99f179b42e6afd515ad053aeac43
Abe <abhi.rawat456@gmail.com>
2020-12-30 13:02:03 +0530

    Added navigation guards

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,43 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import GameDetails from "./components/GameDetails";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import Home from "./components/Home";
-import RootReducer from "./store/reducers";
 import Auth from "./components/Auth";
 import Favorites from "./components/Favorites";
+import { UserInfoState } from "./store/reducers/types";
 
-const store = createStore(RootReducer, applyMiddleware(thunk));
 const queryClient = new QueryClient();
 
-function App() {
+export default function App() {
+  const userId = useSelector(
+    (state: { userInfo: UserInfoState }) => state.userInfo.uid
+  );
+
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Navbar />
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/search" component={Search} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/favorites" component={Favorites} />
-            <Route path="/:id" component={GameDetails} />
-          </Switch>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/search" component={Search} />
+          <Route
+            path="/auth"
+            render={(props) =>
+              userId ? <Redirect to="/" /> : <Auth {...props} />
+            }
+          />
+          <Route
+            path="/favorites"
+            render={() => (userId ? <Favorites /> : <Redirect to="/" />)}
+          />
+          <Route path="/:id" component={GameDetails} />
+        </Switch>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import "./index.css";
 import App from "./App";
+import RootReducer from "./store/reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(RootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
